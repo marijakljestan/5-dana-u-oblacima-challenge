168c08376f017defe1633feaf15ee9cd028967ea
Marija Klještan <marijakljestan@uns.ac.rs>
2022-11-15 11:23:56 +0100

    Initial model

---
+++ backend/challenge/src/main/java/com/hakaton/challenge/domain/Order.java
@@ -0,0 +1,26 @@
+package com.hakaton.challenge.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    private int id;
+    private String currencyPair = "BTCUSD";
+    private Date createdDateTime;
+    private OrderType type;
+    private Double price;
+    private Double quantity;
+    private Double filledQuantity;
+    private OrderStatus orderStatus;
+    private List<Trade> trades;
+}
---
+++ backend/challenge/src/main/java/com/hakaton/challenge/domain/OrderBook.java
@@ -0,0 +1,19 @@
+package com.hakaton.challenge.domain;
+
+import com.hakaton.challenge.dto.OrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderBook {
+
+    private List<OrderDto> buyOrders;
+    private List<OrderDto> sellOrders;
+}
