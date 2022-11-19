2d0e96c8789531840fcef276f700822b56f95b2f
Lule321 <62907894+Lule321@users.noreply.github.com>
2022-11-19 12:29:59 +0100

    Added User

---
+++ backend/challenge/src/main/java/com/hakaton/challenge/domain/UserEntity.java
@@ -0,0 +1,35 @@
+package com.hakaton.challenge.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user_entity")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+    @Id
+    @SequenceGenerator(name = "userSeqGen", sequenceName = "userSeqGen", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userSeqGen")
+    @Column(name="user_id", unique=true, nullable=false)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @OneToMany(mappedBy = "user")
+    private List<OrderEntity> orders;
+}
--- backend/challenge/src/main/resources/data-postgres.sql
+++ backend/challenge/src/main/resources/data-postgres.sql
@@ -1,13 +1,16 @@
+INSERT INTO user_entity(user_id, email, firstname, lastname)
+VALUES(nextval('user_seq_gen'), 'ime@gmail.com', 'ime', 'prezime');
+
 -- orders
-INSERT INTO order_entity (order_id, created_date_time, currency_pair, filled_quantity, order_status, price, quantity, type)
-VALUES (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 7.0, 250.0, 0),
-       (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 9.0, 100.0, 1),
-       (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 10.0, 50.0, 1),
-       (nextval('order_seq_gen'), '2022-05-24 16:49:05.591', 'BTCUSD', 0.0, 0, 11.0, 1000.0, 1),
-       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 15.0, 250.0, 1),
-       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 6.0, 800.0, 0),
-       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 5.0, 150.0, 0),
-       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 1.0, 5000.0, 0);
+INSERT INTO order_entity (order_id, created_date_time, currency_pair, filled_quantity, order_status, price, quantity, type, user_user_id)
+VALUES (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 7.0, 250.0, 0, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 9.0, 100.0, 1, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:48:05.591', 'BTCUSD', 0.0, 0, 10.0, 50.0, 1, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:49:05.591', 'BTCUSD', 0.0, 0, 11.0, 1000.0, 1, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 15.0, 250.0, 1, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 6.0, 800.0, 0, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 5.0, 150.0, 0, 1),
+       (nextval('order_seq_gen'), '2022-05-24 16:38:05.591', 'BTCUSD', 0.0, 0, 1.0, 5000.0, 0, 1);
 
 -- trades
 --INSERT INTO trade_entity (trade_id, buy_order_id, created_date_time, price, quantity, sell_order_id)
